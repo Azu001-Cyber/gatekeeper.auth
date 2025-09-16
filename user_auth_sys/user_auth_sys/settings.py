28db20516b51ce0565720d7e4b39bf34c51b43ff
cybercodemomo <cybercodemomo@gmail.com>
2025-09-16 15:55:04 +0100

    Set STATIC_ROOT outside DEBUG block

--- user_auth_sys/user_auth_sys/settings.py
+++ user_auth_sys/user_auth_sys/settings.py
@@ -133,9 +133,10 @@ USE_L10N = True
 
 STATIC_URL = '/static/'
 
+# Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
